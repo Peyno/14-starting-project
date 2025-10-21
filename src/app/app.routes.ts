7cb132281e8773b1ae0baf70be4d1155b651b18f
Peyno <75258235+Peyno@users.noreply.github.com>
2025-10-21 15:29:10 +0200

    finishing Video 280

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,31 @@
 import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
+import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
+import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes: Routes = [
     {
-        path: 'tasks', // <your-domain>/tasks
-        component: TasksComponent,
+        path: '', // <your-domain>/
+        component: NoTaskComponent
     },
+    {
+        path: 'users/:userId', //<your-domain>/users/<uid>
+        component: UserTasksComponent, 
+        children: [
+            {
+                path: 'tasks', // <your-domain>/users/<uid>/tasks
+                component: TasksComponent 
+            }, 
+            {
+                path: 'tasks/new', 
+                component: NewTaskComponent
+            }
+        ]
+    },
+    {
+        path: '**',
+        component: NotFoundComponent,
+    }
 ]
